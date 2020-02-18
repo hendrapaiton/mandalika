c5d06e502b0b8b55b3be0ebc7a87f7896bd0d914
Hendra Dwi Saputra <hendra@usa.com>
2020-02-18 18:23:59 +0700

    serve static file (index.html)

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='./static')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
     app.run()
