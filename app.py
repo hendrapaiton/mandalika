d960808b8efe147fa5181c11affe1241868ab679
Hendra Dwi Saputra <hendra@usa.com>
2020-03-05 01:57:06 +0700

    enable CORS for Development (turn off for Production)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Import from system libraries
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
@@ -20,6 +21,8 @@ app.config.from_envvar('ENV_FILE_LOCATION')
 bcrypt = Bcrypt(app)
 # JWT instances
 jwt = JWTManager(app)
+# CORS enabled
+CORS(app)
 
 
 # Get roles for authenticated user
